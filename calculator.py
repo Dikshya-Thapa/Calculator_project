05247bebfc6a6fb47f8bca2f109768156a7db51b
ashiyaaa121 <khatriashiya16@gmail.com>
2026-03-30 12:14:01 +0545

    Addition is completed

--- calculator.py
+++ calculator.py
@@ -1,1 +1,8 @@
+def add_numbers(num1, num2):
+    result = num1 + num2
+    return result
 
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+print("Result:", add_numbers(a, b)) 
